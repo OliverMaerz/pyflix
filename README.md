8d46f8e260262d0f137abdbb96261ce02d8cc440
Oliver Maerz <github@olivermaerz.com>
2016-12-04 19:15:31 -0300

    Add files for initial version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pyflix
+
+A little python script to show favorite movies. 
+
+To run the script:
+
+- Install python (in case you do not have it installed yet)
+- Clone https://github.com/OliverMaerz/pyflix.git
+- In the pyflix directory run `python entertainment_center.py`
+
+Et voilà!
+
+
---
+++ media.py
@@ -0,0 +1,9 @@
+class Movie():
+	# Movie class for objects holding datails about movies (like title, poster image url, trailer url etc.)
+
+	def __init__(self, movie_title, movie_storyline, poster_image, details_url, trailer_youtube):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.details_url = details_url
+		self.trailer_youtube_url = trailer_youtube
